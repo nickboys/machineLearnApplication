0593063f0c7d85b92de399350479c1d4e9f5ad8f
nickboys <878251087@qq.com>
2018-04-05 16:01:46 +0800

    knn

--- com/knn/kNN.py
+++ com/knn/kNN.py
@@ -3,8 +3,9 @@
 
 from numpy import *
 import operator
-import matplotlib
-import matplotlib.pyplot as plt
+from os import listdir
+
+
 
 def createDataSet():
     group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
@@ -27,50 +28,93 @@ def classify0(inX, dataSet, labels, k):
     sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
 
-def filematrix(filename):
-    fr=open(filename)
-    arrayOLines=fr.readlines()
-    numberOflines=len(arrayOLines)
-    returnMat=zeros((numberOflines,3))
-    classLabelVector=[]
-    index=0
-    for line in arrayOLines:
-        line=line.strip()
-        listFromLine=line.split("\t")
-        returnMat[index,:]=listFromLine[0:3]
-        classLabelVector.append((listFromLine[-1]))
-        index+=1
-    return returnMat,classLabelVector
 
-def autonorm(dataSet):
-    minVals=dataSet.min(0)
-    maxVals=dataSet.max(0)
-    ranges=maxVals-minVals
-    normDataSet=zeros(shape(dataSet))
-    m=dataSet.shape[0]
-    normDataSet=dataSet-tile(minVals,(m,1))
-    normDataSet=normDataSet/tile(ranges,(m,1))
-    return normDataSet,ranges,minVals
+def filematrix(filename):
+    fr = open(filename)
+    arrayOLines = fr.readlines()
+    numberOflines = len(arrayOLines)
+    returnMat = zeros((numberOflines, 3))
+    classLabelVector = []
+    index = 0
+    for line in arrayOLines:
+        line = line.strip()
+        listFromLine = line.split("\t")
+        returnMat[index, :] = listFromLine[0:3]
+        classLabelVector.append((listFromLine[-1]))
+        index += 1
+    return returnMat, classLabelVector
+
+
+def autoNorm(dataSet):
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataSet = zeros(shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = dataSet - tile(minVals, (m, 1))
+    normDataSet = normDataSet / tile(ranges, (m, 1))
+    return normDataSet, ranges, minVals
+
 
 def datingClassTest():
-    hoRatio=0.10
-    datingDataMat,datingLabels=filematrix('datingTestSet2.txt')
-    normMat,ranges,minVals=autonorm(datingDataMat)
-    m=normMat.shape[0]
-    numTestVecs=int(m*hoRatio)
-    errorCount=0.0
+    hoRatio = 0.50  # hold out 10%
+    datingDataMat, datingLabels = filematrix('datingTestSet2.txt')  # load data setfrom file
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m * hoRatio)
+    errorCount = 0.0
     for i in range(numTestVecs):
-        classifierResult=classify0(normMat[i,:],normMat[numTestVecs:m,:],datingLabels[numTestVecs:m],3)
-        print("the classifier came back with :"+classifierResult+",the real answer is :"+datingLabels[i])
-    if (classifierResult != datingLabels[i]): errorCount += 1.0
-    print('the total error rate is :%f' % int(errorCount / numTestVecs))
+        classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m, :], datingLabels[numTestVecs:m], 3)
+        print("the classifier came back with: %d, the real answer is: %d" % (int(classifierResult), int(datingLabels[i])))
+        if (classifierResult != datingLabels[i]): errorCount += 1.0
+    print("the total error rate is: %f" % (errorCount / float(numTestVecs)))
+    print(errorCount)
+
+
+def img2vector(filename):
+    returnVect = zeros((1, 1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0, 32 * i + j] = int(lineStr[j])
+    return returnVect
+
+
+def handwritingClassTest():
+    hwLabels = []
+    trainingFileList = listdir('trainingDigits')  # load the training set
+    m = len(trainingFileList)
+    trainingMat = zeros((m, 1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]  # take off .txt
+        classNumStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        trainingMat[i, :] = img2vector('trainingDigits/%s' % fileNameStr)
+    testFileList = listdir('testDigits')  # iterate through the test set
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]  # take off .txt
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
+        classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
+        print
+        "the classifier came back with: %d, the real answer is: %d" % (classifierResult, classNumStr)
+        if (classifierResult != classNumStr): errorCount += 1.0
+    print
+    "\nthe total number of errors is: %d" % errorCount
+    print
+    "\nthe total error rate is: %f" % (errorCount / float(mTest))
 
 
 if __name__ == '__main__':
     group, labels = createDataSet()
     classify0([0, 0], group, labels, 3)
     print(classify0([0, 1.1], group, labels, 3))
-    datingDataMat, datingLabels =filematrix('datingTestSet2.txt')
+    datingDataMat, datingLabels = filematrix('datingTestSet2.txt')
     # print(datingDataMat,datingLabels)
     # fig = plt.figure();
     # ax = fig.add_subplot(111)
